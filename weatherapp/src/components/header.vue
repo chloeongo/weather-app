bd55873414c1c9b409fdf5472ab05d16cb07a5b3
chloeongo <chloe.ongolesono@gmail.com>
2025-01-08 21:26:46 +0100

    svg header
    maan svg in header gezet voor een darkmode toggle

--- weatherapp/src/components/header.vue
+++ weatherapp/src/components/header.vue
@@ -3,6 +3,7 @@
     <img src="/logo.svg" />
     <a href="">Home</a>
     <a href="">Weer</a>
+    <img id="modeToggle" src="/moon.svg" />
   </header>
 </template>
 
@@ -16,15 +17,22 @@ export default {
 img {
   width: 70px;
   height: 70px;
-  margin-right: 50px;
+  margin-right: 80px;
   margin-left: 10px;
+}
+#modeToggle {
+  margin-top: 20px;
+  width: 28px;
+  height: 28px;
+  margin-right: 0;
+  margin-left: 202%;
 }
 header {
   display: flex;
   justify-content: left;
 }
 header a {
-  margin-top: 20px;
+  margin-top: 25px;
   margin-right: 90px;
 }
 a {
